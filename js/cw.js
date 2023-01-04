b8042d03d06a718147b756cc5196060a1c7f1a48
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-01-04 22:28:31 +0200

    c

--- js/cw.js
+++ js/cw.js
@@ -76,3 +76,57 @@
 
 // **************************
 
+/*  Дано список цілих чисел, визначте, парною чи непарною є сума його елементів.
+
+Дайте свою відповідь у вигляді відповідного рядка "odd"або "even".
+
+Якщо вхідний масив порожній, розглядайте його як: [0](масив із нулем).
+
+приклади:
+Input: [0]
+Output: "even"
+
+Input: [0, 1, 4]
+Output: "odd"
+
+Input: [0, -1, -5]
+Output: "even"
+*/
+
+function oddOrEven(array) {
+    // return (array.reduce((pV, b) => {return pV + b}, 0) / array.length) % 2 === 0 ? 'even' : 'odd';
+    const number = array.reduce((pV, b) => { return pV + b }, 0);
+    console.log(number);
+}
+
+
+console.log(oddOrEven([0]));
+// , 'even')
+console.log(oddOrEven([1]));
+// , 'odd')
+console.log(oddOrEven([]));
+// , 'even')
+console.log(oddOrEven([0, 1, 5]));
+// , 'even')
+console.log(oddOrEven([0, 1, 3]));
+// , 'even')
+console.log(oddOrEven([1023, 1, 2]));
+// , 'even')
+console.log(oddOrEven([0, -1, -5]));
+// , 'even')
+console.log(oddOrEven([0, -1, -3]));
+//  'even')
+console.log(oddOrEven([-1023, 1, -2]));
+// , 'even')
+console.log(oddOrEven([0, 1, 2]));
+// , 'odd')
+console.log(oddOrEven([0, 1, 4]));
+// , 'odd')
+console.log(oddOrEven([1023, 1, 3]));
+// , 'odd')
+console.log(oddOrEven([0, -1, 2]));
+// , 'odd')
+console.log(oddOrEven([0, 1, -4]));
+// , 'odd')
+console.log(oddOrEven([-1023, -1, 3]));
+// , 'odd')
