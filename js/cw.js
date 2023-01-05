0169f26466df422c252e075a4feab587f11b52bb
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-01-05 19:13:59 +0200

    сщььше

--- js/cw.js
+++ js/cw.js
@@ -148,22 +148,150 @@ uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
 uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
 uniqueInOrder([1,2,2,3,3])       == [1,2,3] */
 
-// var uniqueInOrder=function(iterable){
-//   //your code here - remember iterable can be a string or an array
-// }
+// var uniqueInOrder = function (iterable) {
+
+// };
 
 
 // console.log(uniqueInOrder('AAAABBBCCDAABBB'));
+// console.log(uniqueInOrder(''));
+// console.log(uniqueInOrder(NaN));
+// console.log(uniqueInOrder('a,,bbbbb,,,,,c,,,'));
+
 
 // // // Відповідь
 
-// var uniqueInOrder=function(iterable){
-//     const newAr = []
-//     const newIterable = iterable.split('')
-//     for (let i = -1; i < newIterable.length; i += 1) {;
-//         if (iterable[i] !== newIterable[i + 1]) {
-//             newAr.push(newIterable[i])
+// var uniqueInOrder = function (iterable) {
+//     const newArr = [];
+//     const newIt = iterable.split('');
+//     for (let i = 0; i < newIt.length; i += 1) {
+//         if (newIt[i] !== newIt[i + 1]) {
+//             newArr.push(newIt[i])
+//         }
+
+//     }
+//     return newArr;
+// };
+
+
+// **************************
+
+/*
+ * Напиши скрипт для відображення часу дедлайну здачі проекту. Використовуй конструкцію if...else.
+* Якщо до дедлайну 0 днів - виведи рядок "Сьогодні"
+* Якщо до дедлайну 1 день - виведи рядок "Завтра"
+* Якщо до дедлайну 2 дні - виведи рядок "Післязавтра"
+* Якщо до дедлайну 3+ днів - виведи рядок "Дата в майбутньому"
+ */
+
+// const daysUntilDeadline = 2;
+
+// if (daysUntilDeadline === 0) {
+//     alert("Сьогодні")
+// } else if (daysUntilDeadline === 1) {
+//     alert("Завтра")
+// } else if (daysUntilDeadline === 2) {
+//     alert("Післязавтра")
+// } else {
+//     alert("Дата в майбутньому")
+// }
+
+// switch (daysUntilDeadline) {
+//     case 0:
+//         alert("Сьогодні")
+//         break;
+//     case 1:
+//         alert("Завтра")
+//         break;
+//     case 2:
+//         alert("Завтра")
+//         break;
+//     default: alert("Дата в майбутньому")
+// }
+
+// **************************
+
+//Напишіть функцію removeItem(arr, num), яка видаляє певний елемент із масиву.
+
+// function removeItem(arr, num) {
+//     const newArr = [];
+//     for (let elem of arr) {
+//         if (elem !== num) {
+//             newArr.push(elem);
 //         }
 //     }
-//     return newAr
+//     return newArr;
 // }
+
+// console.log(removeItem([3, 5, 7, 8, 5], 5)); // [3,7,8]
+
+// **************************
+
+/*
+ * Напишіть ф-цію calcTotalPrice(array, stoneName), яка приймає масив об'єктів та рядок з назвою каменю. Ф-ція рахує і повертає загальну вартість каміння з таким ім'ям, ціною та кількістю з об'єкта
+ */
+
+// const stones = [
+//     { name: 'Ізумруд', price: 1300, quantity: 4 },
+//     { name: 'Бриліант', price: 2700, quantity: 3 },
+//     { name: 'Сапфір', price: 400, quantity: 7 },
+//     { name: 'Щебень', price: 200, quantity: 2 },
+// ];
+
+// function calcTotalPrice(array, stoneName) {
+//     for (let el of array) {
+//         if (el.name === stoneName) {
+//             return el.price * el.quantity
+//         }
+//     }
+//     return 'not found'
+// }
+
+
+
+
+// console.log(calcTotalPrice(stones, 'Ізумруд'));
+// console.log(calcTotalPrice(stones, 'Цегла'));
+
+// **************************
+
+// Цього разу ні історії, ні теорії. Наведені нижче приклади показують, як написати функцію accum:
+
+// приклади:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// Параметр accum — це рядок, який містить лише літери з a..zі A..Z.
+
+// function accum(s) {
+// }
+
+// console.log(accum("ZpglnRxqenU"));
+// // "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+// console.log(accum("NyffsGeyylB"));
+// // "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+// console.log(accum("MjtkuBovqrU"));
+// // "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+// console.log(accum("EvidjUnokmM"));
+// // "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+// console.log(accum("HbideVbxncC"));
+// // "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+
+
+// // Відповідь
+// function accum(s) {
+//     let str = []
+//     for (let i = 0; i < s.split('').length; i++) {
+//         if (i !== 0) { str.push('-') }
+//         for (let j = 0; j <= i; j++) {
+//             if (j === 0) {
+//                 str.push(s.split('')[i].toUpperCase())
+//             } else if (j !== 0) {
+//                 str.push(s.split('')[i].toLowerCase())
+//             }
+//         }
+//     }
+//     return str.join('')
+// }
+
+//*********************
