cc65f62ae3356d0509a2047454b2554ac8f394bc
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-01-31 14:16:29 +0200

    commit

--- js/cw.js
+++ js/cw.js
@@ -1618,3 +1618,24 @@ X123456788   -->  false */
 // });
 
 //*********************
+// * Є функція, яка генерує випадкові числа від 1 до 4.
+//  * Написати функцію, яка повертає проміс.
+//  * Зробіть так, щоб згенероване число було затримкою функції setTimeout в секундах.
+//  * Оберніть все це в проміс, який у будь-якому разі повертає час затримки (і в resolve, і в reject).
+//  * Нехай проміс виконається успішно, якщо згенеровано 1 або 2 (`✅ Resolved after ${delay} sec`), і з помилкою - якщо 3 або 4 (`❌ Rejected after ${delay} sec`).
+//  */
+
+// const getRandomNumber = () => Math.floor(Math.random() * 4) + 1;
+// function makePromise() {
+//     return new Promise((resolve, reject) => {
+//         const random = getRandomNumber();
+//         setTimeout(() => {
+//             if (random < 3) {
+//                 resolve(`✅ Resolved after ${random} sec`);
+//             } else {
+//                 reject(`❌ Rejected after ${random} sec`);
+//             }
+//         }, random * 1000);
+//     });
+// }
+// makePromise().then(console.log).catch(console.log);
